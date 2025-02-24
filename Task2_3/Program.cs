fffc332b4e02d3647e0a1423363cc64bb00db8cf
Artem Myagtin <MyagtinArt@outlook.com>
2025-02-24 23:24:45 +0300

    Task2_3

---
+++ Task2_3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число дюймов: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            double b = a * 2.54;
+
+            int meters = (int)b / 1000;
+            int santimeters = (int)b % 100 /10;
+            double millimeters = b % 10;
+
+            Console.WriteLine("метры" + meters + "; сантиметры" + santimeters + "; миллиметры = " + millimeters);
+
+            Console.WriteLine("Нажмите любую клавишу");
+            Console.ReadKey();
+        }
+    }
+}
