65cf127daf22982a1b3adb2333ff3ac368d3f49a
Artem Myagtin <MyagtinArt@outlook.com>
2025-02-24 22:31:46 +0300

    Task2_1

---
+++ Task2_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите третье число c: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("a = " + a + "; b = " + b + "; c = " + c);
+
+            Console.Write("Переворачиваем:");
+            int t = c;
+            c = b; b = a; a = t;
+            Console.WriteLine("a = " + a + "; b = " + b + "; c = " + c);
+
+            Console.WriteLine("Нажмите любую клавишу");
+            Console.ReadKey();
+        }
+    }
+}
