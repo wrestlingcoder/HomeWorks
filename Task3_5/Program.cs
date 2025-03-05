5ecedda8dd5e4f8cd521820acb5a56c2a9da09be
Artem <miagtinam@pik.ru>
2025-03-06 02:28:42 +0300

    Task3_5

---
+++ Task3_5/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите первое число a: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        if (a % 10 == 0)
+        {
+            Console.WriteLine("Да");
+        }
+        else
+        {
+            Console.WriteLine("Нет");
+        }
+
+    }
+}
