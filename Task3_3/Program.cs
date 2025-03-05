2e0a1aecec95c26b2e3db10e14a557ef76cb618a
Artem <miagtinam@pik.ru>
2025-03-06 02:07:57 +0300

    Task3_3

---
+++ Task3_3/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите первое число a: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите второе число b: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите второе число c: ");
+        int c = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите второе число d: ");
+        int d = Convert.ToInt32(Console.ReadLine());
+
+
+        if (a <= b && c <= b && d <= b)
+        {
+            Console.WriteLine(b);
+        }
+        else
+        if (b <= c && a <= c && d <= c)
+        {
+            Console.WriteLine(c);
+        }
+        else
+        if (b <= d && a <= d && c <= d)
+        {
+            Console.WriteLine(d);
+        }
+        else
+        {
+            Console.WriteLine(a);
+        }
+
+        Console.WriteLine("Нажмите любую клавишу");
+        Console.ReadKey();
+    }
+}
