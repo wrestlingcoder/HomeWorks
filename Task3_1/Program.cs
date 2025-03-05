38583e6feaeb07f57c9817576192bb526a59fdd8
Artem <miagtinam@pik.ru>
2025-03-05 22:34:19 +0300

    HW lesson3

---
+++ Task3_1/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите первое число a: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите второе число b: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Нажмите любую клавишу");
+        Console.ReadKey();
+    }
+}
