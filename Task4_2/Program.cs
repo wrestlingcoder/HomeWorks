5f21e05efd981a3afa1dd61f3b7d3eb776d50705
Artem <miagtinam@pik.ru>
2025-03-13 02:23:27 +0300

    fx

---
+++ Task4_2/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Вводится натуральное число n. Найти 1 + 1/2 + 1/3 + … + 1/n
+
+        Console.Write("Введите натуральное число n: ");
+        double n = Convert.ToDouble(Console.ReadLine());
+        double s = 0;
+
+        for (double i = 1; i <= n; i++)
+        {
+            s += (1/i);
+        }
+        Console.WriteLine($"1/n! = {s}");
+    }
+}
---
+++ Task4_3/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Вводятся целые числа a>0, b>0. Найти a^b. Не использовать класс Math
+
+
+        Console.Write("Введите натуральное число a: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите натуральное число b: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        double s = 1;
+
+        for (double i = 1; i <= b; i++)
+        {
+            s = s * a ;
+        }
+        Console.WriteLine($"a^b = {s}");
+    }
+}
