e270fcdc07139ead15adfbf94f544a57c107fe8a
Artem Myagtin <MyagtinArt@outlook.com>
2025-03-19 22:56:14 +0300

    HW5

---
+++ Task5_1/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        const int n = 5;
+        int[,] t = new int[n, n];
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                t[i, j] = i == j ? 1 : 0;
+                Console.Write("{0} ", t[i, j]);
+            }
+            Console.WriteLine();    
+        }
+        Console.ReadKey();
+    }
+}
