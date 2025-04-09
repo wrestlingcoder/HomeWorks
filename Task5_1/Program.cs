c7c70c61232196395fbc580dada3a287a112875b
Artem <miagtinam@pik.ru>
2025-04-09 16:59:07 +0300

    5.1

--- Task5_1/Program.cs
+++ Task5_1/Program.cs
@@ -2,18 +2,24 @@
 {
     static void Main(string[] args)
     {
-        const int n = 5;
-        int[,] t = new int[n, n];
 
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                t[i, j] = i == j ? 1 : 0;
-                Console.Write("{0} ", t[i, j]);
-            }
-            Console.WriteLine();    
-        }
-        Console.ReadKey();
+        const int n = 10;
+        int[] array = new int[10]; //массив из 10
+
+
+
+        ////const int n = 5;
+        ////int[,] t = new int[n, n];
+        ////
+        ////for (int i = 0; i < n; i++)
+        ////{
+        ////    for (int j = 0; j < n; j++)
+        ////    {
+        ////        t[i, j] = i == j ? 1 : 0;
+        ////        Console.Write("{0} ", t[i, j]);
+        ////    }
+        ////    Console.WriteLine();    
+        ////}
+        ////Console.ReadKey();
     }
 }
